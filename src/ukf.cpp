b425e0b4dc19f8c145368f6b31e582bf057eb1f3
Imaccer <mcnallyian@hotmail.com>
2024-07-17 07:34:35 -0700

    added member function to generate sigma points

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -71,6 +71,7 @@ void UKF::Prediction(double delta_t) {
    * Modify the state vector, x_. Predict sigma points, the state, 
    * and the state covariance matrix.
    */
+  GenerateAugmentedSigmaPoints();
 }
 
 void UKF::UpdateLidar(MeasurementPackage meas_package) {
@@ -89,4 +90,43 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
    * covariance, P_.
    * You can also calculate the radar NIS, if desired.
    */
+void UKF::GenerateAugmentedSigmaPoints()
+{
+  // create augmented state vector
+  VectorXd x_aug = VectorXd(n_aug_);
+
+  // create augmented state covariance matrix
+  MatrixXd P_aug = MatrixXd(n_aug_, n_aug_);
+
+  // create augmented sigma point matrix
+  MatrixXd Xsig_aug = MatrixXd(n_aug_, 2 * n_aug_ + 1);
+
+  // create augmented mean state
+  x_aug.head(n_x_) = x_;
+  x_aug(5) = 0;
+  x_aug(6) = 0;
+
+  // create augmented covariance matrix
+  MatrixXd Q = MatrixXd(2, 2);
+  Q << std_a_ * std_a_, 0,
+      0, std_yawdd_ * std_yawdd_;
+
+  P_aug.fill(0.0);
+  P_aug.topLeftCorner(n_x_, n_x_) = P_;
+  P_aug.bottomRightCorner(2, 2) = Q;
+
+  // create square root matrix
+  MatrixXd A = P_aug.llt().matrixL();
+
+  // create augmented sigma points
+  Xsig_aug.col(0) = x_aug;
+  // set remaining sigma points
+  for (int i = 0; i < n_aug_; ++i)
+  {
+    Xsig_aug.col(i + 1) = x_aug + sqrt(lambda_ + n_aug_) * A.col(i);
+    Xsig_aug.col(i + 1 + n_aug_) = x_aug - sqrt(lambda_ + n_aug_) * A.col(i);
+  }
+
+  Xsig_ = Xsig_aug;
+}
 }
